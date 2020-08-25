921ea1a9fb86e96db5ecf07669f3ce5b76a86650
aravinthraja <aravinth.raja@anywhere.co>
2020-08-25 14:49:49 +0530

    storybook fix

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,15 +2,14 @@ import { addons } from '@storybook/addons';
 import yourTheme from './yourTheme';
 
 addons.setConfig({
-    isFullscreen: false,
-    showNav: true,
-    showPanel: true,
-    panelPosition: 'bottom',
-    sidebarAnimations: true,
-    enableShortcuts: true,
-    isToolshown: true,
-    theme: undefined,
-    selectedPanel: yourTheme,
-    initialActive: 'sidebar',
-    showRoots: false,
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'bottom',
+  sidebarAnimations: true,
+  enableShortcuts: true,
+  isToolshown: true,
+  theme: undefined,
+  initialActive: 'sidebar',
+  showRoots: false,
 });
